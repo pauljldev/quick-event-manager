e57fe1b7a4250a2f54a78650264fe8c5f55e807e
alanef <alan@fullworks.net>
2022-10-17 12:41:29 +0100

    9.5.1.01: release to repo

--- business/class-business.php
+++ business/class-business.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright (c) 2020.
  * @author            Alan Fuller (support@fullworks)
@@ -20,135 +21,128 @@
  *     You should have received a copy of the GNU General Public License
  *     along with  this plugin.  https://www.gnu.org/licenses/gpl-3.0.en.html
  */
-
 namespace Quick_Event_Manager\Plugin\Business;
 
-use WP_Query;
-use WP_REST_Server;
-
-class Business {
-
-	private $plugin_name;
-	private $version;
-	/**
-	 * @param Freemius $freemius Object for freemius.
-	 */
-	private $freemius;
-
-	public function __construct( $plugin_name, $version, $freemius ) {
-		$this->plugin_name = $plugin_name;
-		$this->version     = $version;
-		$this->freemius    = $freemius;
-	}
-
-	public function hooks() {
-		add_action( 'rest_api_init', array( $this, 'rest_api_init' ) );
-		add_action( 'init', array( $this, 'cron_jobs' ) );
-		add_action( 'qem_pending_cleardown', array( $this, 'pending_cleardown' ) );
-	}
-
-	public function rest_api_init() {
-		register_rest_route(
-			$this->plugin_name . '/v1',
-			'/stripe/',
-			array(
-				'methods'             => WP_REST_Server::CREATABLE,
-				'callback'            => array( $this, 'controller' ),
-				'permission_callback' => '__return_true',
-			)
-		);
-	}
-
-	public function controller( $request ) {
-		$update = new Stripe_Webhooks( $this->plugin_name, $this->version, $this->freemius, $request );
-
-		return $update->run();
-	}
-
-	public function cron_jobs() {
-		if ( ! wp_next_scheduled( 'qem_pending_cleardown' ) ) {
-			wp_schedule_event( time(), 'hourly', 'qem_pending_cleardown' );
-		}
-	}
-
-	public function pending_cleardown() {
-		$this->payment = qem_get_stored_payment();
-		if ( ! isset( $this->payment['usependingcleardown'] )) {
-			return;
-		}
-		if ( 'checked' != $this->payment['usependingcleardown'] ) {
-			return;
-		}
-		$args  = array(
-			'post_type'      => 'event',
-			'post_status'    => 'publish',
-			'posts_per_page' => - 1,
-		);
-		$query = new WP_Query( $args );
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				$id      = get_the_ID();
-				$cost    = get_post_meta( $id, 'event_cost', true );
-				if ( empty($cost) ) {
-					continue;
-				}
-				$paypal = get_post_meta( $id, 'event_paypal', true );
-				if ( ! $paypal ) {
-					continue;
-				}
-				$message = get_option( 'qem_messages_' . $id );
-				if ( ! empty( $message ) ) {
-					foreach ( $message as $key => $attendee ) {
-						if ( 'Paid' != qem_get_element( $attendee, 'ipn', false ) ) {
-							if ( time() - qem_get_element( $attendee, 'datetime_added', time() ) > 1800 ) {
-								unset( $message[ $key ] );
-								if ( ! empty( $this->payment['pendingcleardownmsg'] ) ) {
-									$this->send_notification( qem_get_element( $attendee, 'youremail', false ), qem_get_element( $attendee, 'yourname', false ) );
-								}
-							}
-						}
-					}
-					$message = array_values( $message );
-					update_option( 'qem_messages_' . $id, $message );
-				}
-			}
-		}
-		wp_reset_postdata();
-
-	}
-
-	private function send_notification( $youremail, $yourname ) {
-		global $post;
-		if ( ! $youremail ) {
-			return;
-		}
-		$register = qem_get_stored_register();
-		$subject  = __( 'Payment not completed for', 'quick-event-manager' ) . get_the_title() . __( ' on ', 'quick-event-manager' ) . get_the_date();
-		$headers  = "Reply-To: " . $yourname . " <" . $youremail . ">\r\n"
-		            . "Content-Type: text/html; charset=\"utf-8\"\r\n";
-		$content  = '<p>' . $this->payment['pendingcleardownmsg'] . '</p>';
-		$content  .= '<p><a href="' . get_the_permalink() . '">' . get_the_permalink() . '</a></p>';
-		$message  = '<html>' . $content . '</html>';
-
-		if ( $register['qemmail'] == 'smtp' ) {
-			qem_send_smtp(
-				$youremail,
-				$subject,
-				array(
-					'yourname'  => $yourname,
-					'youremail' => $youremail,
-				),
-				$message );
-		} else {
-			add_filter( 'wp_mail_content_type', array( $this, 'set_html_content_type' ) );
-			wp_mail( $youremail, $subject, $message );
-			remove_filter( 'wp_mail_content_type', array( $this, 'set_html_content_type' ) );
-		}
-	}
-
-	public function set_html_content_type() {
-		return 'text/html';
-	}
+use  WP_Query ;
+use  WP_REST_Server ;
+class Business
+{
+    private  $plugin_name ;
+    private  $version ;
+    /**
+     * @param Freemius $freemius Object for freemius.
+     */
+    private  $freemius ;
+    public function __construct( $plugin_name, $version, $freemius )
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        $this->freemius = $freemius;
+    }
+    
+    public function hooks()
+    {
+        add_action( 'rest_api_init', array( $this, 'rest_api_init' ) );
+        add_action( 'init', array( $this, 'cron_jobs' ) );
+        add_action( 'qem_pending_cleardown', array( $this, 'pending_cleardown' ) );
+    }
+    
+    public function rest_api_init()
+    {
+    }
+    
+    public function cron_jobs()
+    {
+        if ( !wp_next_scheduled( 'qem_pending_cleardown' ) ) {
+            wp_schedule_event( time(), 'hourly', 'qem_pending_cleardown' );
+        }
+    }
+    
+    public function pending_cleardown()
+    {
+        $this->payment = qem_get_stored_payment();
+        if ( !isset( $this->payment['usependingcleardown'] ) ) {
+            return;
+        }
+        if ( 'checked' != $this->payment['usependingcleardown'] ) {
+            return;
+        }
+        $args = array(
+            'post_type'      => 'event',
+            'post_status'    => 'publish',
+            'posts_per_page' => -1,
+        );
+        $query = new WP_Query( $args );
+        if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                $id = get_the_ID();
+                $cost = get_post_meta( $id, 'event_cost', true );
+                if ( empty($cost) ) {
+                    continue;
+                }
+                $paypal = get_post_meta( $id, 'event_paypal', true );
+                if ( !$paypal ) {
+                    continue;
+                }
+                $message = get_option( 'qem_messages_' . $id );
+                
+                if ( !empty($message) ) {
+                    foreach ( $message as $key => $attendee ) {
+                        if ( 'Paid' != qem_get_element( $attendee, 'ipn', false ) ) {
+                            
+                            if ( time() - qem_get_element( $attendee, 'datetime_added', time() ) > 1800 ) {
+                                unset( $message[$key] );
+                                if ( !empty($this->payment['pendingcleardownmsg']) ) {
+                                    $this->send_notification( qem_get_element( $attendee, 'youremail', false ), qem_get_element( $attendee, 'yourname', false ) );
+                                }
+                            }
+                        
+                        }
+                    }
+                    $message = array_values( $message );
+                    update_option( 'qem_messages_' . $id, $message );
+                }
+            
+            }
+        }
+        wp_reset_postdata();
+    }
+    
+    private function send_notification( $youremail, $yourname )
+    {
+        global  $post ;
+        if ( !$youremail ) {
+            return;
+        }
+        $register = qem_get_stored_register();
+        $subject = __( 'Payment not completed for', 'quick-event-manager' ) . get_the_title() . __( ' on ', 'quick-event-manager' ) . get_the_date();
+        $headers = "Reply-To: " . $yourname . " <" . $youremail . ">\r\n" . "Content-Type: text/html; charset=\"utf-8\"\r\n";
+        $content = '<p>' . $this->payment['pendingcleardownmsg'] . '</p>';
+        $content .= '<p><a href="' . get_the_permalink() . '">' . get_the_permalink() . '</a></p>';
+        $message = '<html>' . $content . '</html>';
+        
+        if ( $register['qemmail'] == 'smtp' ) {
+            qem_send_smtp(
+                $youremail,
+                $subject,
+                array(
+                'yourname'  => $yourname,
+                'youremail' => $youremail,
+            ),
+                $message
+            );
+        } else {
+            add_filter( 'wp_mail_content_type', array( $this, 'set_html_content_type' ) );
+            wp_mail( $youremail, $subject, $message );
+            remove_filter( 'wp_mail_content_type', array( $this, 'set_html_content_type' ) );
+        }
+    
+    }
+    
+    public function set_html_content_type()
+    {
+        return 'text/html';
+    }
 
 }
--- control/autoloader.php
+++ control/autoloader.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright (c) 2020.
  * @author            Alan Fuller (support@fullworks)
@@ -21,57 +22,56 @@
  *     along with  this plugin.  https://www.gnu.org/licenses/gpl-3.0.en.html
  */
 spl_autoload_register(
-/**
- * @param $class_name
- */
-	function ( $class_name ) {
-		/**
-		 * @var \Freemius $qem_fs Object for freemius.
-		 */
-		global $qem_fs;
-		/**
-		 * Autoloader based on tutorial by
-		 * Tom McFarlin https://tommcfarlin.com/  Licensed    GPL-2.0+
-		 */
-		if ( false === strpos( $class_name, 'Quick_Event_Manager\Plugin' ) ) {
-			return;
-		}
-		$file_parts = explode( '\\', $class_name );
-		$namespace  = '';
-		for ( $i = count( $file_parts ) - 1; $i > 1; $i -- ) { // >1 as we ignore the last bit
-			$current = strtolower( $file_parts[ $i ] );
-			$current = str_ireplace( '_', '-', $current );
-			if ( count( $file_parts ) - 1 === $i ) {
-				if ( strpos( strtolower( $file_parts[ count( $file_parts ) - 1 ] ), 'interface' ) ) {
-					$interface_name      = explode( '_', $file_parts[ count( $file_parts ) - 1 ] );
-					$interface_name      = $interface_name[0];
-					$file_name_no_suffix = "interface-$interface_name";
-				} else {
-					$file_name_no_suffix = "class-$current";
-				}
-			} else {
-				$namespace = '/' . $current . $namespace;
-			}
-		}
-		$filepath = trailingslashit( dirname( dirname( __FILE__ ) ) . $namespace );
-		$filepath .= $file_name_no_suffix;
-		if ( file_exists( $filepath . '.php' ) ) {
-			include_once( $filepath . '.php' );
-
-			return;
-		}
-		if ( null !== $qem_fs && $qem_fs->can_use_premium_code__premium_only() ) {
-			// allow premium only class files to be loaded
-			if ( file_exists( $filepath . '__premium_only.php' ) ) {
-				include_once( $filepath . '__premium_only.php' );
-
-				return;
-			}
-		}
-		// changed to be translatable
-		$msg = sprintf( esc_html__( 'The system file attempting to be loaded at %1$s does not exist.', 'quick-event-manager' ), esc_html( $filepath ) . ' ' . esc_html($class_name) );
-		trigger_error( $msg, E_USER_NOTICE );
-		wp_die( $msg );
-
-	}
+    /**
+     * @param $class_name
+     */
+    function ( $class_name ) {
+        /**
+         * @var \Freemius $qem_fs Object for freemius.
+         */
+        global  $qem_fs ;
+        /**
+         * Autoloader based on tutorial by
+         * Tom McFarlin https://tommcfarlin.com/  Licensed    GPL-2.0+
+         */
+        if ( false === strpos( $class_name, 'Quick_Event_Manager\\Plugin' ) ) {
+            return;
+        }
+        $file_parts = explode( '\\', $class_name );
+        $namespace = '';
+        for ( $i = count( $file_parts ) - 1 ;  $i > 1 ;  $i-- ) {
+            // >1 as we ignore the last bit
+            $current = strtolower( $file_parts[$i] );
+            $current = str_ireplace( '_', '-', $current );
+            
+            if ( count( $file_parts ) - 1 === $i ) {
+                
+                if ( strpos( strtolower( $file_parts[count( $file_parts ) - 1] ), 'interface' ) ) {
+                    $interface_name = explode( '_', $file_parts[count( $file_parts ) - 1] );
+                    $interface_name = $interface_name[0];
+                    $file_name_no_suffix = "interface-{$interface_name}";
+                } else {
+                    $file_name_no_suffix = "class-{$current}";
+                }
+            
+            } else {
+                $namespace = '/' . $current . $namespace;
+            }
+        
+        }
+        $filepath = trailingslashit( dirname( dirname( __FILE__ ) ) . $namespace );
+        $filepath .= $file_name_no_suffix;
+        
+        if ( file_exists( $filepath . '.php' ) ) {
+            include_once $filepath . '.php';
+            return;
+        }
+        
+        if ( null !== $qem_fs ) {
+        }
+        // changed to be translatable
+        $msg = sprintf( esc_html__( 'The system file attempting to be loaded at %1$s does not exist.', 'quick-event-manager' ), esc_html( $filepath ) . ' ' . esc_html( $class_name ) );
+        trigger_error( $msg, E_USER_NOTICE );
+        wp_die( $msg );
+    }
 );
